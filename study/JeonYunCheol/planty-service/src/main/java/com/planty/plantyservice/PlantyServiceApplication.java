ccaba4d43768848fbca5c684c4d4b8bafbe7c7ea
전윤철 <tlfldntm4444@naver.com>
2023-07-26 16:08:30 +0900

    [BE] Add : JeonYunCheol - 'planty-service' 프로젝트 생성

--- study/JeonYunCheol/planty-service/src/main/java/com/planty/plantyservice/PlantyServiceApplication.java
+++ study/JeonYunCheol/planty-service/src/main/java/com/planty/plantyservice/PlantyServiceApplication.java
@@ -1,13 +1,13 @@
-package com.planty.discoveryservice;
+package com.planty.plantyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiscoveryServiceApplication {
+public class PlantyServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServiceApplication.class, args);
+		SpringApplication.run(PlantyServiceApplication.class, args);
 	}
 
 }
