bf751c8feec08dcd6ddbe9b4649a4fac207e36ff
GoldGyu <lkg2097@naver.com>
2023-08-04 17:32:30 +0900

    [FE] Add  : 구독 상세페이지 추가개발

--- frontend/src/components/organisms/common/InfoList/InfoList.tsx
+++ frontend/src/components/organisms/common/InfoList/InfoList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import InfoListItem from 'components/atoms/common/InfoListItem/InfoListItem';
-
+import Level from 'components/organisms/shop/Level/Level';
 /**
  * 구독, 상품 등의 정보를 나타내는 아이템의 모음
  * @param info 정보들을 담고 있는 객체
@@ -8,13 +8,17 @@ import InfoListItem from 'components/atoms/common/InfoListItem/InfoListItem';
 function InfoList({ info }: { info: object }) {
 	const keys = Object.keys(info);
 	const values = Object.values(info);
+	const period = info;
+	console.log(period);
+	console.log(keys);
+	console.log(values);
 
 	return (
 		<div className="info-list-container">
 			{keys.map((title, idx) => (
 				<InfoListItem key={title}>
 					<span>{title}</span>
-					<span>{values[idx]}</span>
+					{title === 'level' ? <Level level={values[idx]} /> : <span>{values[idx]}</span>}
 				</InfoListItem>
 			))}
 		</div>
--- frontend/src/components/organisms/shop/Level/Level.tsx
+++ frontend/src/components/organisms/shop/Level/Level.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import LeafFill from 'assets/icons/LeafFill.svg';
 import Leaf from 'assets/icons/Leaf.svg';
+import './Level.scss';
 
 /**
  * 구독샵 상품의 난이도를 나타내는 나뭇잎.
  * @param level 상품에 포함된 식물의 난이도
  */
 function Level({ level }: { level: number }) {
-	const filled = new Array(level).fill(<img src={LeafFill} alt="" />);
-	const another = new Array(5 - level).fill(<img src={Leaf} alt="" />);
+	const filled = new Array(level).fill(<img src={LeafFill} alt="" className="leaf-img" />);
+	const another = new Array(5 - level).fill(<img src={Leaf} alt="" className="leaf-img" />);
 
 	return (
 		<div className="level-container">
