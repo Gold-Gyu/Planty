f4e70ee58f4e663e99db39222d064fed2aab4685
임희선 <huiseon4107@gmail.com>
2023-08-08 23:17:24 +0900

    [FE] Chore : 화상 연결 로딩 페이지에 돌아가기 버튼 추가

--- frontend/src/components/layout/Page/ConsultingLoadingPageLayout/ConsultingLoadingPageLayout.tsx
+++ frontend/src/components/layout/Page/ConsultingLoadingPageLayout/ConsultingLoadingPageLayout.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './ConsultingLoadingPageLayout.scss';
 import LoadingSpinner from 'components/atoms/consulting/LoadingSpinner/LoadingSpinner';
+import useMovePage from 'hooks/useMovePage';
+import { ReactComponent as BackIcon } from 'assets/icons/Back.svg';
 
 function ConsultingLoadingPageLayout() {
+	const { goBack } = useMovePage();
+
+	const handleGoBack = () => {
+		goBack();
+	};
+
 	return (
 		<div className="loading-page-layout">
 			<div className="text">그린메이트와 연결 중이예요</div>
 			<LoadingSpinner />
+			<div className="btn-wrap">
+				<button type="button" onClick={handleGoBack}>
+					<BackIcon />
+					<p>돌아가기</p>
+				</button>
+			</div>
 		</div>
 	);
 }
